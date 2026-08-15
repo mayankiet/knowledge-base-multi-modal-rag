732b9ca8ef3423dfe2e570dfa4c5189c19e53f79
Princy Sharma <princysharma@Princys-MacBook-Air.local>
2026-08-15 21:47:18 +0530

    updated rag.py

--- rag.py
+++ rag.py
@@ -1,12 +1,16 @@
 from uuid import uuid4
 from dotenv import load_dotenv
 from pathlib import Path
-from langchain.chains import RetrievalQAWithSourcesChain
+
 from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
