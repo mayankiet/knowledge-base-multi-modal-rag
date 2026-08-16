c1fe5fed93b1c17adc3c9a90db19dbaac6d88433
Princy Sharma <princysharma@Princys-MacBook-Air.local>
2026-08-16 17:58:25 +0530

    resolving streamlit issue

--- voiceAssisstentRag.py
+++ voiceAssisstentRag.py
@@ -13,8 +13,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-
-from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
